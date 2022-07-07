c3076da18c34bdf940f62a5d4b23cdf7077c5f22
scene <376124750@qq.com>
2022-07-07 12:37:05 +0800

    xx

---
+++ debunced/src/main/java/com/github/sceneren/debunced/CheckViewOnClick.kt
@@ -0,0 +1,10 @@
+package com.github.sceneren.debunced
+
+/**
+ * @Author: leavesCZY
+ * @Date: 2021/12/4 18:02
+ * @Desc:
+ */
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class CheckViewOnClick
